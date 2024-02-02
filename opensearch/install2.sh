2b0d829781c2382911f72beade11f7b0df45ed4b
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-02-02 06:23:53 -0800

    Update install2.sh

--- opensearch/install2.sh
+++ opensearch/install2.sh
@@ -56,26 +56,26 @@ printf "\n\n\n*********Sleeping 20 seconds to give dpkg time to clean up...\n\n"
 sleep 20s
 
 #Install the necessary packages.
-sudo apt-get update && sudo apt-get -y install lsb-release ca-certificates curl gnupg2
+ apt-get update &&  apt-get -y install lsb-release ca-certificates curl gnupg2
 
 #Import the public GPG key. This key is used to verify that the APT repository is signed.
-curl -o- https://artifacts.opensearch.org/publickeys/opensearch.pgp | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/opensearch-keyring
+curl -o- https://artifacts.opensearch.org/publickeys/opensearch.pgp |  gpg --dearmor --batch --yes -o /usr/share/keyrings/opensearch-keyring
 
 #Create an APT repository for OpenSearch:
-echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.opensearch.org/releases/bundle/opensearch/2.x/apt stable main" | sudo tee /etc/apt/sources.list.d/opensearch-2.x.list
+echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.opensearch.org/releases/bundle/opensearch/2.x/apt stable main" |  tee /etc/apt/sources.list.d/opensearch-2.x.list
 
 #Verify that the repository was created successfully. #Unless otherwise indicated, the latest available version of OpenSearch is installed.
-sudo apt-get update && sudo apt-get -y install opensearch
+ apt-get update &&  apt-get -y install opensearch
 
 #With the repository information added, list all available versions of OpenSearch:
-#sudo apt list -a opensearch
+# apt list -a opensearch
 
 #To install a specific version of OpenSearch:
 # Specify the version manually using opensearch=<version>
-#sudo apt-get install opensearch=2.11.1
+# apt-get install opensearch=2.11.1
 
 #Once complete, enable OpenSearch.
-sudo systemctl enable opensearch && systemctl start opensearch
+ systemctl enable opensearch && systemctl start opensearch
 
 #test
 #curl -X GET https://localhost:9200 -u 'admin:admin' --insecure
@@ -83,7 +83,7 @@ sudo systemctl enable opensearch && systemctl start opensearch
 
 printf "\n\n\n*********Configuring OpenSearch - set 0.0.0.0 as network.host\n\n"
 config_path="/etc/opensearch/opensearch.yml"
-replace_text "$config_path" "#network.host: 192.168.0.1" "network.host: 0.0.0.0" "${LINENO}"
+replace_text "$config_path" "#network.host: 192.168.0.1" "network.host: localhost" "${LINENO}"
 
 # Bind OpenSearch to the correct network interface. Use 0.0.0.0
 # to include all available interfaces or specify an IP address
@@ -93,7 +93,7 @@ replace_text "$config_path" "#network.host: 192.168.0.1" "network.host: 0.0.0.0"
 # Unless you have already configured a cluster, you should set
 # discovery.type to single-node, or the bootstrap checks will
 # fail when you try to start the service.
-echo "discovery.type: single-node" | sudo tee -a "$config_path"
+echo "discovery.type: single-node" |  tee -a "$config_path"
 
 # If you previously disabled the Security plugin in opensearch.yml,
 # be sure to re-enable it. Otherwise you can skip this setting.
@@ -105,26 +105,26 @@ config_path="/etc/opensearch/jvm.options"
 replace_text "$config_path" '## -Xms4g' '-Xms4g' "${LINENO}"
 replace_text "$config_path" '## -Xmx4g' '-Xmx4g' "${LINENO}"
 
-sudo systemctl enable opensearch && systemctl start opensearch
+ systemctl enable opensearch && systemctl start opensearch
 
 #############install opensearch dashboards
 #Install the necessary packages.
-sudo apt-get update && sudo apt-get -y install lsb-release ca-certificates curl gnupg2
+ apt-get update &&  apt-get -y install lsb-release ca-certificates curl gnupg2
 
 #Import the public GPG key. This key is used to verify that the APT repository is signed.
-curl -o- https://artifacts.opensearch.org/publickeys/opensearch.pgp | sudo gpg --dearmor --batch --yes -o /usr/share/keyrings/opensearch-keyring
+curl -o- https://artifacts.opensearch.org/publickeys/opensearch.pgp |  gpg --dearmor --batch --yes -o /usr/share/keyrings/opensearch-keyring
 
-echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.opensearch.org/releases/bundle/opensearch-dashboards/2.x/apt stable main" | sudo tee /etc/apt/sources.list.d/opensearch-dashboards-2.x.list
+echo "deb [signed-by=/usr/share/keyrings/opensearch-keyring] https://artifacts.opensearch.org/releases/bundle/opensearch-dashboards/2.x/apt stable main" |  tee /etc/apt/sources.list.d/opensearch-dashboards-2.x.list
 
-sudo apt-get update && sudo apt-get -y install opensearch-dashboards
-#sudo apt list -a opensearch-dashboards
+ apt-get update &&  apt-get -y install opensearch-dashboards
+# apt list -a opensearch-dashboards
 
 config_path="/etc/opensearch-dashboards/opensearch_dashboards.yml"
 #Specify a network interface that OpenSearch Dashboards should bind to.
 # Use 0.0.0.0 to bind to any available interface.
 replace_text "$config_path" '# server.host: "localhost"' 'server.host: "0.0.0.0"' "${LINENO}"
 
-sudo systemctl enable opensearch-dashboards && systemctl start opensearch-dashboards
+ systemctl enable opensearch-dashboards && systemctl start opensearch-dashboards
 
 printf "\n\n\n*********Downloading and installing ElastiFlow Flow Collector...\n\n" 
 #Install Elastiflow flow collector
@@ -140,7 +140,7 @@ replace_text "$path" 'Environment="EF_OUTPUT_OPENSEARCH_ECS_ENABLE=false"' 'Envi
 replace_text "$path" 'Environment="EF_OUTPUT_OPENSEARCH_TLS_ENABLE=false"' 'Environment="EF_OUTPUT_OPENSEARCH_TLS_ENABLE=true"' "${LINENO}"
 replace_text "$path" 'Environment="EF_OUTPUT_OPENSEARCH_TLS_SKIP_VERIFICATION=false"' 'Environment="EF_OUTPUT_OPENSEARCH_TLS_SKIP_VERIFICATION=true"' "${LINENO}"
 
-sudo systemctl enable flowcoll.service && systemctl start flowcoll.service
+ systemctl enable flowcoll.service && systemctl start flowcoll.service
 
 printf "\n\n\n*********Downloading and installing ElastiFlow flow dashboards\n\n"
 git clone https://github.com/elastiflow/elastiflow_for_opensearch.git /etc/elastiflow_for_opensearch/
@@ -151,7 +151,7 @@ git clone https://github.com/elastiflow/elastiflow_for_opensearch.git /etc/elast
 curl -k -XPUT -H'content-type: application/json' https://admin:admin@localhost:9200/_plugins/_security/api/tenants/tenant_one -d '{"description": "tenant one"}'
 #curl -k -XPUT -H'content-type: application/json' https://admin:admin@localhost:9200/_plugins/_security/api/tenants/tenant_two -d '{"description": "tenant two"}'
 
-#login to opensearch-dashboards and save the cookie. note the current tenant is __user__, i guess the value for private
+#login to opensearch-dashboards and save the cookie.
 curl -k -XGET -u 'admin:admin' -c dashboards_cookie http://localhost:5601/api/login/
 curl -k -XGET -b dashboards_cookie http://localhost:5601/api/v1/configuration/account | jq
 
@@ -171,13 +171,6 @@ else
 fi
 
 printf "\n\nGo to http://host_ip:5601 (admin / admin)\n\n"
+printf "Use \"Tenant 1\""
 
 printf "\n\n\n*********\nAll done.\n\n"
-
-printf "\n\n\n*********Clean up - Command history purged\n\n"
-history -c && history -w
-
-printf "\n\n\n*********Clean up - Deleting setup files...\n\n"
-rm -rf ~/elastiflow_update/
-rm -rf ~/ef_va/
-
