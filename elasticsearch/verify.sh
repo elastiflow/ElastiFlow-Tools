a459075246e872fb96b10fa78ec6a660aeb0690a
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-02-28 20:21:26 -0800

    Update verify.sh

--- elasticsearch/verify.sh
+++ elasticsearch/verify.sh
@@ -2,6 +2,8 @@
 
 interface="ens32"
 
+#!/bin/bash
+
 function verifyElastiFlow() {
     read -p "Do you want to verify if ElastiFlow is receiving and sending flow to Elastic? (y/n) " answer
     if [[ ${answer:0:1} =~ [yY] ]]; then
@@ -17,43 +19,41 @@ function verifyElastiFlow() {
         echo "pmacct installed successfully."
 
         # Configure pmacct
-        interface=$interface
         sudo mkdir -p /etc/pmacct/
-        echo "daemonize: true
-        pcap_interface: $interface
-        aggregate: src_mac, dst_mac, src_host, dst_host, src_port, dst_port, proto, tos
-        plugins: nfprobe, print
-        nfprobe_receiver: localhost:9995
-        nfprobe_version: 9
-        nfprobe_timeouts: tcp=15:maxlife=1800" | tee /etc/pmacct/pmacctd.conf
-        
+        sudo bash -c "cat > /etc/pmacct/pmacctd.conf" <<EOF
+daemonize: true
+pcap_interface: $interface
+aggregate: src_mac, dst_mac, src_host, dst_host, src_port, dst_port, proto, tos
+plugins: nfprobe, print
+nfprobe_receiver: localhost:9995
+nfprobe_version: 9
+nfprobe_timeouts: tcp=15:maxlife=1800
+EOF
+
         # Start pmacctd
         echo "Starting pmacctd..."
         sudo pmacctd -f /etc/pmacct/pmacctd.conf &
         PMACCT_PID=$!
 
-        sleep 3
-         
-        echo "Generating network traffic - wget google.com"
+        echo "Generating network traffic (wget google.com)."
         for i in {1..5}; do
             wget -qO- http://google.com > /dev/null
         done
 
         # Terminate pmacct after 35 seconds
-        echo -n "Terminating pmacct in 35 seconds..."
+        echo -n "Terminating pmacct in 35 seconds... "
         for i in {35..1..-1}; do
-            echo -ne "\r$i seconds remaining..."
+            echo -ne "\rTerminating pmacct in $i seconds... "
             sleep 1
         done
 
-        echo -e "\rTerminating pmacct...         "
+        echo -e "\rTerminating pmacct...              "
         sudo kill $PMACCT_PID
 
-        # Query Kibana for ElastiFlow indices
-        echo "Querying Kibana for ElastiFlow indices..."
-        ELASTIFLOW_INDICES=$(curl -s "http://localhost:5601/api/console/proxy?path=_cat/indices%2Felastiflow-*&method=GET" | jq -r '.[] | select(.index | contains("elastiflow")) | .index')
-
-        if [[ ! -z "$ELASTIFLOW_INDICES" ]]; then
+        # Query Elasticsearch for ElastiFlow indices
+        echo "Querying Elasticsearch for ElastiFlow indices..."
+        response=$(curl -k -s -u elastic:elastic "http://localhost:9200/_cat/indices?format=json")
+        if echo $response | jq -e '.[] | select(.index | contains("elastiflow"))' >/dev/null; then
             echo "ElastiFlow has been successfully verified."
         else
             echo "Something's wrong. No ElastiFlow indices found."
@@ -64,3 +64,4 @@ function verifyElastiFlow() {
 }
 
 verifyElastiFlow
+
