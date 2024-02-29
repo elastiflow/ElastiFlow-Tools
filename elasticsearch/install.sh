0bc9cf0f9890485265929dc809ff098b376e79a2
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-02-28 21:16:00 -0800

    Update install.sh

--- elasticsearch/install.sh
+++ elasticsearch/install.sh
@@ -19,64 +19,6 @@ replace_text() {
     sed -i.bak "s|$old_text|$new_text|g" "$file_path" || handle_error "Failed to replace text in $file_path." "$line_num"
 }
 
-function verifyElastiFlow() {
-    read -p "Do you want to verify if ElastiFlow is receiving and sending flow to Elastic? (y/n) " answer
-    if [[ ${answer:0:1} =~ [yY] ]]; then
-        echo "Installing pmacct..."
-         apt-get update &&  apt-get install pmacct -y
-        
-        # Verify pmacct installation
-        if ! command -v pmacctd &> /dev/null; then
-            echo "pmacct installation failed."
-            return 1
-        fi
-        
-        echo "pmacct installed successfully."
-
-        # Configure pmacct
-         mkdir -p /etc/pmacct/
-         bash -c "cat > /etc/pmacct/pmacctd.conf" <<EOF
-daemonize: true
-pcap_interface: $interface
-aggregate: src_mac, dst_mac, src_host, dst_host, src_port, dst_port, proto, tos
-plugins: nfprobe, print
-nfprobe_receiver: localhost:9995
-nfprobe_version: 9
-nfprobe_timeouts: tcp=15:maxlife=1800
-EOF
-
-        # Start pmacctd
-        echo "Starting pmacctd..."
-         pmacctd -f /etc/pmacct/pmacctd.conf &
-
-        echo "Generating network traffic (wget google.com)."
-        for i in {1..5}; do
-            wget -qO- http://google.com > /dev/null
-        done
-
-        # Terminate pmacct after 35 seconds
-        echo -n "Terminating pmacct in 35 seconds... "
-        for i in {35..1..-1}; do
-            echo -ne "\rTerminating pmacct in $i seconds... "
-            sleep 1
-        done
-
-        echo -e "\rTerminating pmacct...              "
-         killall pmacctd
-
-        # Query Elasticsearch for ElastiFlow indices
-        echo "Querying Elasticsearch for ElastiFlow indices..."
-        response=$(curl -k -s -u elastic:elastic "https://localhost:9200/_cat/indices?format=json")
-        if echo $response | jq -e '.[] | select(.index | contains("elastiflow"))' >/dev/null; then
-            echo -e "\e[32mElastiFlow has been successfully verified.\e[0m"
-        else
-            echo "Something's wrong. No ElastiFlow indices found."
-        fi
-    else
-        echo "Verification cancelled."
-    fi
-}
-
 
 if [ "$(id -u)" -ne 0 ]; then
     echo "This script must be run as root" 1>&2
