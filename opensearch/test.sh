7049f4391dd9562242da10900bbc8241b2182404
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-03-31 16:11:57 -0700

    Create test.sh

---
+++ opensearch/test.sh
@@ -0,0 +1,11 @@
+version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:9200/" | jq -r '.version.number')
+print "Installed OpenSearch Version: $version\n"
+
+version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:5601/api/status" | jq -r '.version.number')
+print "Installed OpenSearch Dashboards Version: $version\n"
+
+version=$(/usr/share/elastiflow/bin/flowcoll -version)
+printf "Installed ElastiFlow version: $version\n"
+
+version=$(lsb_release -d | awk -F'\t' '{print $2}')
+printf "Operating System: $version\n\n"
