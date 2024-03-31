e857329cb349b04b6ca564829c413dc725ad7a1c
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-03-31 16:30:09 -0700

    Update install.sh

--- opensearch/install.sh
+++ opensearch/install.sh
@@ -184,17 +184,18 @@ else
     printf "Not successful\n\n"
 fi
 
-version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:9200/" | jq -r '.version.number')
-print "Installed OpenSearch Version: $version\n"
+version=$(curl -k -XGET https://admin:"yourStrongPassword123!"@localhost:9200 | jq -r '.version.number')
+printf "Installed OpenSearch Version: $version\n"
 
-version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:5601/api/status" | jq -r '.version.number')
-print "Installed OpenSearch Dashboards Version: $version\n"
+version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:5601/api/status | jq -r '.version.number')
+printf "Installed OpenSearch Dashboards Version: $version\n"
 
 version=$(/usr/share/elastiflow/bin/flowcoll -version)
 printf "Installed ElastiFlow version: $version\n"
 
 version=$(lsb_release -d | awk -F'\t' '{print $2}')
 printf "Operating System: $version\n\n"
+
 
 
 printf "\n\nGo to http://host_ip:5601 (admin / "$OPENSEARCH_INITIAL_ADMIN_PASSWORD")\n\n"
