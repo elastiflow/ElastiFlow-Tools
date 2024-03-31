fecf224efe3051def9e458c0b31241aaef9cdfb2
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-03-31 16:12:19 -0700

    Update test.sh

--- opensearch/test.sh
+++ opensearch/test.sh
@@ -1,3 +1,10 @@
+
+#!/bin/bash
+
+elastiflow_version="6.4.2"
+export OPENSEARCH_INITIAL_ADMIN_PASSWORD="yourStrongPassword123!"
+
+
 version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:9200/" | jq -r '.version.number')
 print "Installed OpenSearch Version: $version\n"
 
