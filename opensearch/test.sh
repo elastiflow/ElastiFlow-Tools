cd56be9992d5382a890156713fd5fa506c13e301
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-03-31 16:14:09 -0700

    Update test.sh

--- opensearch/test.sh
+++ opensearch/test.sh
@@ -7,7 +7,7 @@ export OPENSEARCH_INITIAL_ADMIN_PASSWORD="yourStrongPassword123!"
 version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:9200/ | jq -r '.version.number')
 print "Installed OpenSearch Version: $version\n"
 
-version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:5601/api/status" | jq -r '.version.number')
+version=$(curl -s http://admin:"$OPENSEARCH_INITIAL_ADMIN_PASSWORD"@localhost:5601/api/status | jq -r '.version.number')
 print "Installed OpenSearch Dashboards Version: $version\n"
 
 version=$(/usr/share/elastiflow/bin/flowcoll -version)
