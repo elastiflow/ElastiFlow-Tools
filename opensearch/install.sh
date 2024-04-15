5e7ce32e9293ccb4d19b67ac1d7660e70766ea2c
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-04-14 23:32:36 -0700

    Update install.sh

--- opensearch/install.sh
+++ opensearch/install.sh
@@ -42,7 +42,7 @@ swapoff -a
 # Make a copy of /etc/fstab
 cp /etc/fstab /etc/fstab_backup
 # Prepend a '#' to every line containing the word 'swap' in the backup file
-sed -i '/swap/s/^/#/' /etc/fstab_backup
+sed -i '/swap/s/^/#/' /etc/fstab
 
 printf "\n\n\n*********Configuring JVM memory usage...\n\n"
 # Get the total installed memory from /proc/meminfo in kB
