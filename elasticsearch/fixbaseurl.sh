b5d80343f28caac822af1993b03ee944c09f225e
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-04-29 11:23:24 -0700

    Update fixbaseurl.sh

--- elasticsearch/fixbaseurl.sh
+++ elasticsearch/fixbaseurl.sh
@@ -1,7 +1,4 @@
+#!/bin/bash
 
-
-
-printf "\n\n\n*********Configuring Kibana - set elasticsearch.hosts to localhost instead of DHCP IP...\n\n"
 kibana_config_path="/etc/kibana/kibana.yml"
-replace_text "$kibana_config_path" "elasticsearch.hosts: \['https:\/\/[^']*'\]" "elasticsearch.hosts: \['https:\/\/localhost:9200'\]" "${LINENO}"
 replace_text "$kibana_config_path" '#server.publicBaseUrl: ""' 'server.publicBaseUrl: "http://kibana.example.com:5601"' "${LINENO}"
