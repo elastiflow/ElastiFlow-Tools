38999f39065b6194436a63425f7e9cf156724c76
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-04-29 11:22:19 -0700

    Update install2.sh

--- elasticsearch/install2.sh
+++ elasticsearch/install2.sh
@@ -204,6 +204,8 @@ printf "\n\n\n*********Enrolling Kibana with Elastic...\n\n"
 printf "\n\n\n*********Configuring Kibana - set elasticsearch.hosts to localhost instead of DHCP IP...\n\n"
 kibana_config_path="/etc/kibana/kibana.yml"
 replace_text "$kibana_config_path" "elasticsearch.hosts: \['https:\/\/[^']*'\]" "elasticsearch.hosts: \['https:\/\/localhost:9200'\]" "${LINENO}"
+replace_text "$kibana_config_path" '#server.publicBaseUrl: ""' 'server.publicBaseUrl: "http://kibana.example.com:5601"' "${LINENO}"
+
 
 printf "\n\n\n*********Generating Kibana saved objects encryption key...\n\n"
 # Run the command to generate encryption keys quietly
