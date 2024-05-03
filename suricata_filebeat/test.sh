adeaa14e826b6c660016e23ebcf7c492b8b746fc
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-05-03 08:53:27 -0700

    Update test.sh

--- suricata_filebeat/test.sh
+++ suricata_filebeat/test.sh
@@ -2,7 +2,7 @@
 
 
 
-curl -X GET "http://localhost:9200/filebeat-*/_search" -H 'Content-Type: application/json' -d'
+curl -X GET "http://localhost:9200/filebeat-*/_search" -u username:password -H 'Content-Type: application/json' -d'
 {
   "query": {
     "match_phrase": {
@@ -10,4 +10,5 @@ curl -X GET "http://localhost:9200/filebeat-*/_search" -H 'Content-Type: applica
     }
   },
   "size": 10
-}'
+}' | jq 'if .hits.total.value == 10 then "\u001b[32mfound\u001b[0m" else "\u001b[31mnot found\u001b[0m" end'
+
