0064e63bdf997436b4eeb73a1eaf3b7ea7dac0e7
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-05-14 16:07:36 -0700

    Update elastiflow_elasticsearch_opensearch_support_pack.sh

--- support_pack/elastiflow_elasticsearch_opensearch_support_pack.sh
+++ support_pack/elastiflow_elasticsearch_opensearch_support_pack.sh
@@ -110,12 +110,20 @@ backup_saved_objects() {
 # Output file for the exported saved objects
 OUTPUT_FILE="kibana_saved_objects_backup.ndjson"
 
-# Export all saved objects
-echo "Exporting all saved objects..."
+# Export specific saved objects
+echo "Exporting specific saved objects..."
 curl -s -u "$USERNAME:$PASSWORD" -X POST "$KIBANA_URL/api/saved_objects/_export" -H "kbn-xsrf: true" -H "Content-Type: application/json" -d '{
-  "type": "",
+  "objects": [
+    {"type": "dashboard"},
+    {"type": "visualization"},
+    {"type": "search"},
+    {"type": "index-pattern"},
+    {"type": "map"},
+    {"type": "lens"}
+  ],
   "includeReferencesDeep": true
 }' --output "$OUTPUT_FILE"
+
 
 # Check if the export was successful
 if [ $? -eq 0 ]; then
