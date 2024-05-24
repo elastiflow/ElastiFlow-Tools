b5190969c2b29d7f7aea5ccb5e5baffab4cb7e63
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-05-24 11:08:19 -0700

    Update configure_yml.sh

--- configure/configure_yml.sh
+++ configure/configure_yml.sh
@@ -8,6 +8,75 @@ NC='\033[0m' # No Color
 elastiflow_version="6.4.4"
 
 
+
+
+STRINGS_TO_REPLACE=(
+"EF_LICENSE_ACCEPTED" "EF_LICENSE_ACCEPTED: \"true\""
+)
+
+comment_and_add_line() {
+  local FILE=$1
+  local FIND=$2
+  local REPLACE=$3
+
+  # Check if the line exists and if it's already commented out
+  if grep -q "^$FIND" "$FILE"; then
+    # If the line is not commented out, comment it out and add the new line underneath
+    sed -i.bak "/^$FIND/s/^/#/; /^$FIND/a $REPLACE" "$FILE"
+    echo "Commented out '$FIND' and added '$REPLACE' underneath."
+  elif grep -q "^#$FIND" "$FILE"; then
+    # If the line is already commented out, just add the new line underneath
+    sed -i.bak "/^#$FIND/a $REPLACE" "$FILE"
+    echo "Found commented out line '$FIND'. Added '$REPLACE' underneath."
+  else
+    # Add the line under the heading #ElastiFlow VA installer
+    if grep -q "^#ElastiFlow VA installer" "$FILE"; then
+      sed -i.bak "/^#ElastiFlow VA installer/a $REPLACE" "$FILE"
+      echo "Added '$REPLACE' under the heading '#ElastiFlow VA installer'."
+    else
+      echo "Heading '#ElastiFlow VA installer' not found in the file."
+      echo "Add the heading '#ElastiFlow VA installer' to the file and run the script again."
+      exit 1
+    fi
+  fi
+}
+
+# Function to process an array of find and replace strings
+find_and_replace() {
+  local FILE=$1
+  shift
+  local PAIRS=("$@")
+
+  # Check if the file exists
+  if [ ! -f "$FILE" ]; then
+    echo "File not found!"
+    exit 1
+  fi
+
+  # Loop through the pairs of find and replace strings
+  for ((i = 0; i < ${#PAIRS[@]}; i+=2)); do
+    local FIND=${PAIRS[i]}
+    local REPLACE=${PAIRS[i+1]}
+    comment_and_add_line "$FILE" "$FIND" "$REPLACE"
+  done
+
+  # Verify if the operation was successful
+  for ((i = 0; i < ${#PAIRS[@]}; i+=2)); do
+    local REPLACE=${PAIRS[i+1]}
+    if grep -qF "$REPLACE" "$FILE"; then
+      echo "Verified: '$REPLACE' is in the file."
+    else
+      echo "Verification failed: '$REPLACE' is not in the file."
+    fi
+  done
+}
+
+
+
+
+
+
+
 # Function to check if flowcoll.service exists
 check_service_exists() {
   if ! systemctl list-unit-files | grep -q "flowcoll.service"; then
