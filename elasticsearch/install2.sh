5587e47abbcc093266a703645a52f79e628e1ba8
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-06-06 09:51:55 -0700

    Update install2.sh

--- elasticsearch/install2.sh
+++ elasticsearch/install2.sh
@@ -425,7 +425,7 @@ printf "Installed ElastiFlow version: $version\n"
 version=$(lsb_release -d | awk -F'\t' '{print $2}')
 printf "Operating System: $version\n"
 
-version=$(/usr/lib/jvm/java-21-openjdk-amd64/bin/java -version 2>&1)
+version=$(java -version 2>&1)
 printf "Installed Java version: $version\n\n"
 
 printf "\e[5;37m\n\nGo to http://$IP_ADDRESS:5601/app/dashboards (elastic / elastic)\n\n\e[0m"
