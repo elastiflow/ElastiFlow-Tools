7990e5006b13ccac03175664e6cbe68230713499
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-06-11 11:51:45 -0700

    Create ipaddress.sh

---
+++ elasticsearch/ipaddress.sh
@@ -0,0 +1,20 @@
+get_network_interface_ip() {
+    # Get the first network interface starting with en
+    INTERFACE=$(ip -o link show | grep -o 'en[^:]*' | head -n 1)
+
+    if [ -z "$INTERFACE" ]; then
+        echo ""
+        return 1
+    else
+        # Get the IP address of the interface
+        ip_address=$(ip -o -4 addr show $INTERFACE | awk '{print $4}' | cut -d/ -f1)
+
+        if [ -z "$ip_address" ]; then
+            echo ""
+            return 1
+        else
+            echo "$ip_address"
+            return 0
+        fi
+    fi
+}
