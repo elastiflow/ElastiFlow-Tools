1b3aeca324c41350d06392ebf20dd5b3e81c20a4
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-07-12 08:03:37 -0700

    Delete snmp/install.sh

--- snmp/install.sh
+++
@@ -1,8 +0,0 @@
-
-sudo wget https://elastiflow-releases.s3.us-east-2.amazonaws.com/snmp-collector/snmp-collector_6.4.3_linux_amd64.deb
-sudo apt install libpcap-dev
-sudo apt install ./snmp-collector_6.4.3_linux_amd64.deb
-sudo systemctl daemon-reload && sudo systemctl start snmpcoll.service
-
-
-
