88aa202918bc7dbeae48199373608f7700e32da7
O.J. Wolanyk <88400567+ontech12@users.noreply.github.com>
2024-10-21 14:23:43 -0700

    Update dev.sh

--- docker_install/dev.sh
+++ docker_install/dev.sh
@@ -214,8 +214,7 @@ deploy_elastic_elastiflow_snmp() {
   cd /etc/elastiflow
   git clone https://github.com/elastiflow/snmp.git
   cd "$INSTALL_DIR"
-  docker compose -f elasticsearch_kibana_compose.yml -f elastiflow_flow_compose.yml down
-  docker compose -f elasticsearch_kibana_compose.yml -f elastiflow_flow_compose.yml -f elastiflow_snmp_compose.yml up -d
+  docker compose -f elastiflow_snmp_compose.yml up -d
   install_dashboards "snmp"
   echo "ElastiFlow SNMP Collector has been deployed successfully!"
 }
