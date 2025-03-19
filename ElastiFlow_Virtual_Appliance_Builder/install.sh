c7a632924d936b66ed17263093926f296c66dfe5
Keli (Madison) Grubb <keligrubb324@gmail.com>
2025-03-19 12:49:04 -0400

    Fix gpg import path for unattended installs (#66)
    * ensure home path exists before importing the gpg key
    
    * test directory creation path
    
    * create elastifow user home
    
    * create home directory if in unattended mode

--- ElastiFlow_Virtual_Appliance_Builder/install.sh
+++ ElastiFlow_Virtual_Appliance_Builder/install.sh
@@ -1349,6 +1349,10 @@ install_latest_elastiflow_flow_collector() {
     # Import GPG public key
     if [ -n "$GPG_PUBKEY_FILE" ] && [ -f "$GPG_PUBKEY_FILE" ]; then
         echo "Importing GPG public key..."
+        if $UNATTENDED; then
+          # ensure home directory exists before importing the gpg key
+          mkdir -p /home/elastiflow
+        fi
         gpg --import "$GPG_PUBKEY_FILE" || {
             echo "Warning: Failed to import GPG public key."
             if $UNATTENDED; then
